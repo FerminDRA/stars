13160e5beb50698e36b92e8ab79932d4747eaff9
FerminDRA <fermindelrosarioantonio@gmail.com>
2023-03-06 13:40:42 -0600

    add branch mutation

--- estrellas/schema.py
+++ estrellas/schema.py
@@ -14,3 +14,51 @@ class Query(graphene.ObjectType):
 
     def resolve_estrellas(self, info, **kwargs):
         return Estrella.objects.all()
+
+
+class CreateEstrella(graphene.Mutation):
+    id = graphene.Int()
+    nombre = graphene.String()
+    distancia = graphene.String()
+    radio = graphene.Float()
+    rotacion = graphene.Float()
+    edad = graphene.Int()
+    ubicacion = graphene.Int()
+    masa = graphene.Int()
+    temperatura = graphene.Float()
+    constelacion = graphene.String()
+    color = graphene.String()
+
+
+    class Arguments:
+        nombre = graphene.String()
+        distancia = graphene.String()
+        radio = graphene.Float()
+        rotacion = graphene.Float()
+        edad = graphene.Int()
+        ubicacion = graphene.Int()
+        masa = graphene.Int()
+        temperatura = graphene.Float()
+        constelacion = graphene.String()
+        color = graphene.String()
+
+    def mutate(self, info, nombre, distancia, radio, rotacion, edad, ubicacion, masa, temperatura, constelacion, color):
+        estrella = Estrella(nombre=nombre, distancia=distancia, radio=radio, rotacion=rotacion, edad=edad, ubicacion=ubicacion,temperatura=temperatura, constelacion=constelacion, color=color)
+        estrella.save()
+
+        return CreateEstrella(
+            id=estrella.id,
+            nombre=estrella.nombre,
+            distancia=estrella.distancia,
+            radio=estrella.radio,
+            rotacion=estrella.rotacion,
+            edad=estrella.edad,
+            ubicacion=estrella.ubicacion,
+            masa=estrella.masa,
+            temperatura=estrella.temperatura,
+            constelacion=estrella.constelacion,
+            color=estrella.color,
+        )
+
+class Mutation(graphene.ObjectType):
+    create_estrella = CreateEstrella.Field()
