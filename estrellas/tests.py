e9c692288c30cf388f234fec469519b310808827
FerminDRA <fermindelrosarioantonio@gmail.com>
2023-04-10 21:22:51 -0600

    add test mutation #3

--- estrellas/tests.py
+++ estrellas/tests.py
@@ -19,6 +19,15 @@ ESTRELLAS_QUERY = '''
    }
  }
 '''
+CREATE_ESTRELLA_MUTATION = '''
+ mutation createEstrellaMutation($nombre: String,$distancia: String,$radio: Float,$rotacion: Float,$edad: Int,$ubicacion: Int,$masa: Int,$temperatura: Float,$constelacion: String,$color: String) {
+     createEstrella(nombre: $nombre,distancia: $distancia,radio: $radio,rotacion: $rotacion,edad: $edad,ubicacion: $ubicacion,masa: $masa,temperatura: $temperatura,constelacion: $constelacion,color: $color) {
+        nombre
+     }
+ }
+'''
+#distancia,radio,rotacion,edad,ubicacion,masa,temperatura,constelacion,color
+
 
 class EstrellaTestCase(GraphQLTestCase):
     GRAPHQL_SCHEMA = schema
@@ -39,3 +48,18 @@ class EstrellaTestCase(GraphQLTestCase):
         print ("query estrella results ")
         print (content)
         assert len(content['data']['estrellas']) == 2
+
+    def test_createEstrella_mutation(self):
+
+        response = self.query(
+            CREATE_ESTRELLA_MUTATION,
+            variables={'nombre':'Orion','distancia':'5 ly','radio':5.5,'rotacion':7.8,'edad':5,'ubicacion':15,'masa':7,'temperatura':5000.5,'constelacion':'Orion','color':'Amarillo'}
+        )
+        print('mutation ')
+        print(response)
+        content = json.loads(response.content)
+        print(content)
+        self.assertResponseNoErrors(response)
+        self.assertDictEqual({"createEstrella": {"nombre":"Orion"}}, content['data'])
+        #self.assertDictEqual({"createEstrella": {"nombre":"Orion","distancia":"5 ly"}}, content['data']) 
+#"distancia":"5 ly","radio":5.5,"rotacion":7.8,"edad":5,"ubicacion":15,'masa':7,"temperatura":5000.5,"constelacion":"Orion","color":"Amarillo"
